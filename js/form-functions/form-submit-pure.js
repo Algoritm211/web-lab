9acc01deffcf36e58fa03a00599d4f6b19280683
alexey_horbunov <algoritm211@gmail.com>
2021-03-07 21:34:32 +0200

    Add 4 lab, preparing to 5 lab

---
+++ js/form-functions/form-submit-pure.js
@@ -0,0 +1,87 @@
+import {closePopup, popupOpen} from "./form-functions";
+import $ from 'jquery';
+
+
+
+
+
+
+
+export function formSubmitVanillaJS(form) {
+  const popup = document.querySelector(".popup")
+  popup.classList.add("popup__active")
+
+  const message = {
+    loading: 'Загрузка...',
+    success: 'Спасибо, данные ушли на сервер',
+    failure: 'Что-то пошло не так, попробуйте еще раз'
+  }
+
+  const statusMessage = document.createElement('div')
+  const serverData = document.createElement('div')
+  serverData.classList.add('serverData')
+  serverData.innerHTML = 'На сервер вы отправили такие данные: <br>'
+  statusMessage.classList.add('status')
+  statusMessage.textContent = message.loading
+  popup.querySelector('.popup__data').append(serverData)
+  popup.querySelector('.popup__data').append(statusMessage)
+
+  const request = new XMLHttpRequest();
+
+  request.open('POST', 'server.php')
+  request.setRequestHeader('Content-type', 'application/json; charset=utf-8')
+  // При пост-запросе НЕ ДЛЯ JSON не указывать хедеры
+  const formData = new FormData(form)
+
+  const obj = {}
+
+  // логика для JSON
+  formData.forEach(function (value, key) {
+    obj[key] = value
+  })
+
+  const json = JSON.stringify(obj)
+
+  request.send(json)
+
+  request.addEventListener('load', () => {
+    if (request.status === 200) {
+      console.log(request.response);
+      serverData.innerHTML += request.response
+      statusMessage.textContent = message.success
+      form.reset()
+      setTimeout(() => {
+        statusMessage.remove()
+        serverData.innerHTML = ''
+        statusMessage.textContent = ''
+        closePopup()
+      }, 10000)
+    } else {
+      serverData.innerHTML = ''
+      statusMessage.textContent = message.failure
+    }
+  })
+}
+
+export function formSendJQuery(form) {
+
+  const formData = new FormData(form)
+
+  const obj = {}
+  // логика для JSON
+  formData.forEach(function (value, key) {
+    obj[key] = value
+  })
+
+
+  $.ajax({
+    url: 'server.php',
+    type: "POST",
+    contentType: "application / json",
+    data: obj,
+    success: ()
+  })
+
+
+}
+
